96b003cb4c9cde92c90a197ea17d1416bfc9aa5d
unknown <alfred.chen@netac.com>
2019-01-29 14:43:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img style="width:140px; margin:14px;" src="./assets/vuetube-logo.png"/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
